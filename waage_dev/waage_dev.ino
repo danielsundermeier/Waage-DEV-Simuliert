a5f8f776b3eebbeea2c5816b7d9d61feb5830aa7
danielsundermeier <daniel@hof-sundermeier.de>
2018-11-07 07:18:21 +0100

    dynamisches Datum

--- waage_dev/waage_dev.ino
+++ waage_dev/waage_dev.ino
@@ -1,5 +1,8 @@
 String command = "";
 int laufendeNummer = 1;
+int stunde = 10;
+int minute = 10;
+String datum = "07.11.18";
 
 String randomWeight()
 {
@@ -7,14 +10,22 @@ String randomWeight()
     return "   " + String(number);
 }
 
-String datum()
+String getDatum()
 {
-    return String("17.08.18");
+    return datum;
 }
 
 String zeit()
 {
-    return String(random(10, 24)) + ":" + String(random(10, 59));
+    minute++;
+    if (minute == 60) {
+      stunde++;
+      if (stunde == 24) {
+        stunde = 10;
+      }
+      minute = 10;
+    }
+    return String(stunde) + ":" + String(minute);
 }
 
 String IdentNummer()
@@ -35,7 +46,7 @@ String IdentNummer()
 void antwortGewicht(bool sendIdent = true)
 {
     String weight = randomWeight();
-    Serial.println("<" + String("00") + String("00") + datum() + zeit() + (sendIdent ? IdentNummer() : String("   1")) + String("1") + weight + String("       0") + weight + String("kg") + String("PT") + String(" ") + String("001") + String("   12348") + ">");
+    Serial.println("<" + String("00") + String("00") + getDatum() + zeit() + (sendIdent ? IdentNummer() : String("   1")) + String("1") + weight + String("       0") + weight + String("kg") + String("PT") + String(" ") + String("001") + String("   12348") + ">");
 }
 
 void setup()
@@ -67,6 +78,11 @@ void loop()
         }
         else if (command == "<SZ>") {
             Serial.println("<00>");
+        }
+        else if (command.startsWith("SETDATE")) {
+          command.remove(0, 8);
+          datum = command;
+          Serial.println("Datum setzen: " + datum);
         }
         else
         {
